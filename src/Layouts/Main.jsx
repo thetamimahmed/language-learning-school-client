160219e57a7c09defaf76c45f1f6c8113fba0f86
thetamimahmed <wwwtamim000@gmail.com>
2023-06-09 02:25:06 +0600

    footer added

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,4 +1,5 @@
 import {Outlet} from "react-router-dom"
+import Footer from "../Shared/Footer/Footer";
 import NavBar from "../Shared/NavBar/NavBar";
 
 const Main = () => {
@@ -6,6 +7,7 @@ const Main = () => {
         <div>
             <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
